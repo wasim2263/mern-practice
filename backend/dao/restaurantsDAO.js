530691962240b9ab49c6c36cf6a95678bfe7e53d
wasim <abdullahwasim42@gmail.com>
2021-08-07 11:35:09 +0600

    adding getRestaurants method

--- backend/dao/restaurantsDAO.js
+++ backend/dao/restaurantsDAO.js
@@ -1,15 +1,36 @@
 let restaurants;
 
-export default class RestaurantsDAO{
-    static async injectDB(conn){
-        if (restaurants){
+export default class RestaurantsDAO {
+    static async injectDB(conn) {
+        if (restaurants) {
             return
         }
         try {
             restaurants = await conn.db(process.env.NS).collection('restaurants');
             console.log(restaurants)
-        }catch (e){
+        } catch (e) {
             console.log(e)
         }
+    }
+
+    static async getRestaurants({
+                                    filters = null,
+                                    page = 0,
+                                    restaurantsPerPage: restaurantsPerPage = 20
+                                } = {}) {
+        let query
+        if (filters) {
+            
+        }
+        let cursor
+        try {
+            cursor = await restaurants.find(query)
+        }catch (e) {
+            console.error(e)
+            return{restaurants:[], totalNumberOfRestaurants:0}
+        }
+        const displayCursor = cursor.limit(restaurantsPerPage).skip(restaurants)
+
+
     }
 }
